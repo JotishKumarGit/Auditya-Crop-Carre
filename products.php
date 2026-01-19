3f22be341512f41e44243d2089a92b68c7931333
Jotish <jotishk649@gmail.com>
2026-01-19 14:49:12 +0530

    Added all products and with banner

--- products.php
+++ products.php
@@ -37,6 +37,7 @@
 
             <div class="row justify-content-center align-items-center">
 
+                <!-- 1  -->
                 <div class="col-md-4 col-sm-6">
                     <div class="panel panel-default">
                         <div class="panel-body text-center">
@@ -44,12 +45,12 @@
                                 alt="Chlorpyriphos 50% EC Insecticide">
                             <h3>Chlorpyriphos 50% EC Insecticide</h3>
                             <p>Broad-spectrum insecticide effective against soil and foliar pests in crops.</p>
-                            <a href="#" class="btn btn-default">Details</a>
+                            <a href="Chlorpyriphos.php" class="btn btn-default">Details</a>
                             <a href="#" onclick="openModal()" class="btn btn-primary">Enquiry</a>
                         </div>
                     </div>
                 </div>
-
+                <!-- 2 -->
                 <div class="col-md-4 col-sm-6">
                     <div class="panel panel-default">
                         <div class="panel-body text-center">
@@ -57,25 +58,25 @@
                                 alt="Diafenthiuron 50% WP Insecticide">
                             <h3>Diafenthiuron 50% WP Insecticide</h3>
                             <p>Controls whiteflies, aphids, mites, and other sucking pests effectively.</p>
-                            <a href="#" class="btn btn-default">Details</a>
+                            <a href="Diafenthiuron.php" class="btn btn-default">Details</a>
                             <a href="#" onclick="openModal()" class="btn btn-primary">Enquiry</a>
                         </div>
                     </div>
                 </div>
-
+                <!-- 3 -->
                 <div class="col-md-4 col-sm-6">
                     <div class="panel panel-default">
                         <div class="panel-body text-center">
-                            <img src="./img/Emamectin-Benzoate-Ec-Insecticide.jpeg" style="height: 300px; width: 100%;"
+                            <img src="./img/Emamecting-Benzoate.png" style="height: 300px; width: 100%;"
                                 alt="Emamectin Benzoate 1.9% EC Insecticide">
                             <h3>Emamectin Benzoate 1.9% EC</h3>
                             <p>Highly effective against lepidopteran larvae in vegetables and crops.</p>
-                            <a href="#" class="btn btn-default">Details</a>
+                            <a href="Emamectin-Benzoate.php" class="btn btn-default">Details</a>
                             <a href="#" onclick="openModal()" class="btn btn-primary">Enquiry</a>
                         </div>
                     </div>
                 </div>
-
+                <!-- 4 -->
                 <div class="col-md-4 col-sm-6">
                     <div class="panel panel-default">
                         <div class="panel-body text-center">
@@ -83,25 +84,12 @@
                                 alt="Lambda Cyhalothrin 5% Insecticide">
                             <h3>Lambda Cyhalothrin 5%</h3>
                             <p>Fast-acting synthetic pyrethroid for chewing and sucking insects.</p>
-                            <a href="#" class="btn btn-default">Details</a>
+                            <a href="Lambda-Cyhalothrin.php" class="btn btn-default">Details</a>
                             <a href="#" onclick="openModal()" class="btn btn-primary">Enquiry</a>
                         </div>
                     </div>
                 </div>
-
-                <div class="col-md-4 col-sm-6">
-                    <div class="panel panel-default">
-                        <div class="panel-body text-center">
-                            <img src="./img/Lambdacyalohthrin-Insecticide.jpeg" style="height: 300px; width: 100%;"
-                                alt="Lambda Cyhalothrin 2.5% Insecticide">
-                            <h3>Lambda Cyhalothrin 2.5%</h3>
-                            <p>Provides long-lasting protection against major crop pests.</p>
-                            <a href="#" class="btn btn-default">Details</a>
-                            <a href="#" onclick="openModal()" class="btn btn-primary">Enquiry</a>
-                        </div>
-                    </div>
-                </div>
-
+                <!-- 5 -->
                 <div class="col-md-4 col-sm-6">
                     <div class="panel panel-default">
                         <div class="panel-body text-center">
@@ -109,13 +97,110 @@
                                 alt="Profenophos 50% EC Insecticide">
                             <h3>Profenophos 50% EC Insecticide</h3>
                             <p>Organophosphate insecticide for effective pest management in crops.</p>
-                            <a href="#" class="btn btn-default">Details</a>
+                            <a href="Profenophos.php" class="btn btn-default">Details</a>
+                            <a href="#" onclick="openModal()" class="btn btn-primary">Enquiry</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- 6 -->
+                <div class="col-md-4 col-sm-6">
+                    <div class="panel panel-default">
+                        <div class="panel-body text-center">
+                            <img src="./img/Cypermethrin-ec.png" style="height: 300px; width: 100%;"
+                                alt="Cypermethrin 10% EC Insecticide">
+                            <h3>Cypermethrin 10% EC</h3>
+                            <p>
+                                A powerful synthetic pyrethroid insecticide used to control a wide range of
+                                chewing and sucking pests in cotton, vegetables, and other crops.
+                            </p>
+                            <a href="Cypermethrin.php" class="btn btn-default">Details</a>
+                            <a href="#" onclick="openModal()" class="btn btn-primary">Enquiry</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- 7 -->
+                <div class="col-md-4 col-sm-6">
+                    <div class="panel panel-default">
+                        <div class="panel-body text-center">
+                            <img src="./img/Glyphosate-sl.png" style="height: 300px; width: 100%;" alt="Glyphosate 41% SL Herbicide">
+                            <h3>Glyphosate 41% SL</h3>
+                            <p>
+                                A non-selective systemic herbicide effective against a broad spectrum of
+                                annual and perennial weeds in agricultural and non-crop areas.
+                            </p>
+                            <a href="Glyphosate.php" class="btn btn-default">Details</a>
+                            <a href="#" onclick="openModal()" class="btn btn-primary">Enquiry</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- 8 -->
+                <div class="col-md-4 col-sm-6">
+                    <div class="panel panel-default">
+                        <div class="panel-body text-center">
+                            <img src="./img/Imidacloprid-sl.png" style="height: 300px; width: 100%;" alt="Imidacloprid 17.8% SL Insecticide">
+                            <h3>Imidacloprid 17.8% SL</h3>
+                            <p>
+                                A systemic neonicotinoid insecticide used to control sucking pests such as
+                                aphids, jassids, thrips, and whiteflies in various crops.
+                            </p>
+                            <a href="Imidacloprid.php" class="btn btn-default">Details</a>
+                            <a href="#" onclick="openModal()" class="btn btn-primary">Enquiry</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- 9 -->
+                <div class="col-md-4 col-sm-6">
+                    <div class="panel panel-default">
+                        <div class="panel-body text-center">
+                            <img src="./img/Metalaxyl-mancozeb.png" style="height: 300px; width: 100%;" alt="Metalaxyl 8% + Mancozeb 64% WP Fungicide">
+                            <h3>Metalaxyl 8% + Mancozeb 64% WP</h3>
+                            <p>
+                                A combination fungicide providing both systemic and contact action for the
+                                control of downy mildew, late blight, and other fungal diseases.
+                            </p>
+                            <a href="Metalaxyl-Mancozeb.php" class="btn btn-default">Details</a>
+                            <a href="#" onclick="openModal()" class="btn btn-primary">Enquiry</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- 10 -->
+                <div class="col-md-4 col-sm-6">
+                    <div class="panel panel-default">
+                        <div class="panel-body text-center">
+                            <img src="./img/paraquat-dichloride-sl.png" style="height: 300px; width: 100%;" alt="Paraquat Dichloride 24% SL Herbicide">
+                            <h3>Paraquat Dichloride 24% SL</h3>
+                            <p>
+                                A fast-acting contact herbicide used for effective control of broadleaf weeds
+                                and grasses in plantations and non-crop areas.
+                            </p>
+                            <a href="Paraquat.php" class="btn btn-default">Details</a>
+                            <a href="#" onclick="openModal()" class="btn btn-primary">Enquiry</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- 11 -->
+                <div class="col-md-4 col-sm-6">
+                    <div class="panel panel-default">
+                        <div class="panel-body text-center">
+                            <img src="./img/Thiamethoxam-sg.png" style="height: 300px; width: 100%;" alt="Thiamethoxam 25% WG Insecticide">
+                            <h3>Thiamethoxam 25% WG</h3>
+                            <p>
+                                A systemic neonicotinoid insecticide effective against sucking pests like
+                                aphids, whiteflies, and leafhoppers with long-lasting protection.
+                            </p>
+                            <a href="Thiamethoxam.php" class="btn btn-default">Details</a>
                             <a href="#" onclick="openModal()" class="btn btn-primary">Enquiry</a>
                         </div>
                     </div>
                 </div>
 
             </div>
+
         </div>
     </section>
     <!-- end products sections  -->
